2bd03f2b05fc9f3d91f15335d3a6760738e7138e
McDonald Otoyo <mcdonaldotoyo44@gmail.com>
2022-07-21 15:25:26 +0100

    Created the Hotel class and added .gitignore file

--- registration/admin.py
+++ registration/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from registration.models import Location, Hotel
 
 # Register your models here.
+
+admin.site.register(Location)
+admin.site.register(Hotel)
--- registration/models.py
+++ registration/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 from helpers.models import TrackingModel
 
+class Location(TrackingModel):
+    name = models.CharField(max_length=120)
+    
+    def __str__(self):
+        return f'{ self.name }'
+
 class Hotel(TrackingModel):
     name = models.CharField(max_length=120, null=True)
+    location = models.ForeignKey(Location, null=True, on_delete=models.SET_NULL)
+    hotel_location = models.CharField(max_length=3000, null=True)
+    phone = models.IntegerField(null=True)
+    email = models.EmailField(null=True)
+    number_of_rooms = models.IntegerField(null=True)
+    number_of_booked_rooms = models.IntegerField(null=True)
+    rating = models.IntegerField(null=True)
+    no_rooms_available = models.BooleanField(default=False)
+    date_of_hotel_profile_update = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return f'{ self.name }'
