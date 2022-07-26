e52b0d14f0a0add8990fd598478caa2c08c5d220
McDonald Otoyo <mcdonaldotoyo44@gmail.com>
2022-07-26 20:36:35 +0100

    Made the 'date_to_check_out' to be the next day and i am making a commit of the static files

--- booking/forms.py
+++ booking/forms.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 
 from django import forms
 from django.forms import ModelForm
@@ -13,11 +13,12 @@ from registration.models import Hotel
 
 
 today = date.today()
+tommorow = today + timedelta(days=1)
 
 
 class BookingARoomForm(ModelForm):
 	date_to_check_in = forms.DateField(widget=forms.TextInput(attrs={'min': today, 'value': today, 'type': 'date'}), required=True)
-	date_to_check_out = forms.DateField(widget=forms.TextInput(attrs={'min': today, 'type': 'date'}), required=True)
+	date_to_check_out = forms.DateField(widget=forms.TextInput(attrs={'min': today, 'value': tommorow, 'type': 'date'}), required=True)
 
 
 	class Meta:
