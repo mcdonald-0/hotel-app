ac806c406ba4fc3ba4638845962c5f11de0a874c
McDonald Otoyo <mcdonaldotoyo44@gmail.com>
2022-10-09 04:01:45 +0100

    Removed media urls and files

--- hotel_app/settings.py
+++ hotel_app/settings.py
@@ -179,14 +179,14 @@ USE_TZ = True
 
 STATICFILES_DIRS = [
     (BASE_DIR / 'static'),
-    (BASE_DIR / 'media'),
+    # (BASE_DIR / 'media'),
 ]
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
