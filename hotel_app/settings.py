ceacf67ea7384d9fe09c09157d47a3b5101b1111
McDonald Otoyo <mcdonaldotoyo44@gmail.com>
2022-10-14 05:15:41 +0100

    Removed storages from installed apps

--- hotel_app/settings.py
+++ hotel_app/settings.py
@@ -67,7 +67,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'django_extensions',
     'admin_honeypot',
-    'storages',
+    # 'storages',
 ]
 
 PAYSTACK_PUBLIC_KEY = os.getenv("PAYSTACK_PUBLIC_KEY")
